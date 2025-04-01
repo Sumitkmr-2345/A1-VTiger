8021c80a53e02674393d9e63847af04cae1210a8
Sumit <sumitkmr823003@gmail.com>
2025-04-01 16:11:19 +0530

    branch commit

---
+++ src/test/java/sample/GitPractice.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("from branch....");
+
+	}
+
+}
